a85d5e17bd57b1e25e825a45a453ad775afd2351
Alex <alexxsur@hotmail.com>
2018-03-22 21:15:28 -0500

    Directiva ngIf y evento click

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -10,18 +10,24 @@ export class EmpleadoComponent{
 
     public empleado:Empleado;
     public trabajadores:Array<Empleado>;
+    public trabajador_externo:boolean;
 
     constructor(){
       this.empleado = new Empleado('Ricardo Arjona',78,'Cantante',true);
       this.trabajadores = [
-       new Empleado('Manolo Perez',47,'Manager',true),
+       new Empleado('Manolo Perez',47,'Manager',false),
        new Empleado('Juan Mesa',24,'Mesero',false),
        new Empleado('Sofia Henao',23,'Contadora',true)
       ];
+      this.trabajador_externo = false;
     }
 
 	ngOnInit(){		
 		console.log(this.empleado);
 		console.log(this.trabajadores);
+	}
+
+	cambiarExterno(valor){
+		this.trabajador_externo = valor;
 	}
 }
