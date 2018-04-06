93ff8fbecfa8a72fc102e0beaa04d6484553d16f
Alex <alexxsur@hotmail.com>
2018-04-05 21:20:58 -0500

    Pipes Personalizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { EmpleadoComponent } from './empleado/empleado.component';
 import { HomeComponent } from './home/home.component';
 import { ContactoComponent } from './contacto/contacto.component';
 
+import {ConversorPipe} from './pipes/conversor.pipe';
+
 
 @NgModule({
   declarations: [
@@ -19,7 +21,8 @@ import { ContactoComponent } from './contacto/contacto.component';
     FrutaComponent,
     EmpleadoComponent,
     HomeComponent,
-    ContactoComponent
+    ContactoComponent,
+    ConversorPipe
   ],
   imports: [
     BrowserModule,
