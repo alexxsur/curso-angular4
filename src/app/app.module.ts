fa02bff034829f721699ec3ae5d3b549b3f72572
Alex <alexxsur@hotmail.com>
2018-04-06 19:44:35 -0500

    Crear Formularios ngForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { FrutaComponent } from './fruta/fruta.component';
 import { EmpleadoComponent } from './empleado/empleado.component';
 import { HomeComponent } from './home/home.component';
 import { ContactoComponent } from './contacto/contacto.component';
+import { CochesComponent } from './coches/coches.component';
 
 import {ConversorPipe} from './pipes/conversor.pipe';
 
@@ -22,6 +23,7 @@ import {ConversorPipe} from './pipes/conversor.pipe';
     EmpleadoComponent,
     HomeComponent,
     ContactoComponent,
+    CochesComponent,
     ConversorPipe
   ],
   imports: [
